137af40e71164d89dd18e422ed6b63013539c9ea
pmh905001 <pmh905001@126.com>
2016-02-24 17:54:38 +0800

    as an administrator, I want to start a progress if the progress not exist. First time to add

---
+++ start-progress-if-not-exist/start_progress_if_not_exist.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# usage:start_progress_if_not_exist.sh key start_command
+
+count=`ps -ef | grep $1 | grep -v "grep" | grep -v $0 | wc -l`
+#ps -ef | grep "$1" | grep -v "grep"
+#echo  $1
+#ps -ef | grep "/magichome/xruser/engg/engg_ui" | grep -v "grep"
+if [ $count == 0 ]
+	then
+		echo "will start progress"
+		$2 &
+	else
+		echo "$count, already exist progress"
+fi
+
