25865fd4e220d6b6c9fb0ae56c1b79955566a181
pmh905001 <pmh905001@126.com>
2016-03-01 16:49:29 +0800

    Add usage and example

--- detector-simulator/config-detsim.sh
+++ detector-simulator/config-detsim.sh
@@ -1,74 +1,22 @@
 #! /bin/bash
 
-#Open a terminal
-#su   //to change to superuser
-#service ip
-#	vi /magichome/xruser/config/IDC/Application/ConnectionPoint.cfg
-#Scroll to the bottom,
-#Update IP of detector, like below:
-#iDR:
-#{
-#    eth0:
-#    {
-#        DetectorPanelId = "UNKNOWN";
-#        DetectorType = "UNKNOWN";
-#        Detector_HostPort  = 5550;
-#        Detector_ImagePort = 6660;
-#        DetectorIP = "3.136.143.17";                   #change to real eth1 ip and bcast
-#        Detector_SysIP = "3.136.143.17";
-#        BroadcastAddress = "3.136.143.255";
-#        BroadcastPort = 8100;
-#        BroadcastInterval = 2;
-#        BroadcastTimeout = 20;
-#        SystemPort  = 4500;
-#        EthernetInterface = "eth0";
-#    };
-#}; 
-#	vi /magichome/xruser/config/XRImDet.dyn
-#Update CP_WIFI_1, like below:
-#[CP_WIFI_1]
- 
-#DetcommMedium.Type = String
-#DetcommMedium.Val = eth0
-#PowerSupplyInfo.Type = String
-#PowerSupplyInfo.Val = DCRegulated
-#DetectorConnectionStrategy.Type = Number
-#DetectorConnectionStrategy.Val = 0                     #change from 1 to 0
-#DetectorConnectionType.Type = String
-#DetectorConnectionType.Val = CONNECT_WIFI
-#SupportedDetectorTypes.Type = StringList
-#SupportedDetectorTypes.Val = { iDR }
-#	vi /magichome/xruser/bin/start.magic
-#Add these to the end:
- 
-#to start detsim
-#sudo /magichome/xruser/detsim/bin/tablesim_iDR.sh
- 
-#	vi /magichome/xruser/bin/kilall
-#Add these to the end:
- 
-#to kill detsim
-#sudo /magichome/xruser/detsim/bin/kildetsim.sh
-#	vi /magichome/xruser/install/detector_fw/iDR/VersionInfo.txt
 
-#Change the content to 256f0612
-#	Restart App
-#Kilall;start.magic
-#	when you see detector boots up successfully, execute this command:
-#/magichome/xruser/bin/copyiDRCalFiles.sh
+#This script is used to update the detector simulator.
+#Usage: config-detsim.sh detectorIP broadcastIP
+#For example : config-detsim.sh 3.136.143.68 3.136.143.255
 
 
-path1="ConnectionPoint.cfg"
-path2="XRImDet.dyn"
-path3="start.applications"
-path4="kilall"
-path5="VersionInfo.txt"
+#path1="ConnectionPoint.cfg"
+#path2="XRImDet.dyn"
+#path3="start.applications"
+#path4="kilall"
+#path5="VersionInfo.txt"
 
-#path1="/magichome/xruser/config/IDC/Application/ConnectionPoint.cfg"
-#path2="/magichome/xruser/config/XRImDet.dyn"
-#path3="/magichome/xruser/bin/start.magic"
-#path4="/magichome/xruser/bin/kilall"
-#path5="/magichome/xruser/install/detector_fw/iDR/VersionInfo.txt"
+path1="/magichome/xruser/config/IDC/Application/ConnectionPoint.cfg"
+path2="/magichome/xruser/config/XRImDet.dyn"
+path3="/magichome/xruser/bin/start.magic"
+path4="/magichome/xruser/bin/kilall"
+path5="/magichome/xruser/install/detector_fw/iDR/VersionInfo.txt"
 
 existStr=`grep -c 'iDR:' $path1`
 if [ $existStr == 1 ]
@@ -139,3 +87,73 @@ fi
 #when you see detector boots up successfully, execute this command:
 #/magichome/xruser/bin/copyiDRCalFiles.sh
 
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################################
+#Description
+##############################################################################################
+#Open a terminal
+#su   //to change to superuser
+#service ip
+#       vi /magichome/xruser/config/IDC/Application/ConnectionPoint.cfg
+#Scroll to the bottom,
+#Update IP of detector, like below:
+#iDR:
+#{
+#    eth0:
+#    {
+#        DetectorPanelId = "UNKNOWN";
+#        DetectorType = "UNKNOWN";
+#        Detector_HostPort  = 5550;
+#        Detector_ImagePort = 6660;
+#        DetectorIP = "3.136.143.17";                   #change to real eth1 ip and bcast
+#        Detector_SysIP = "3.136.143.17";
+#        BroadcastAddress = "3.136.143.255";
+#        BroadcastPort = 8100;
+#        BroadcastInterval = 2;
+#        BroadcastTimeout = 20;
+#        SystemPort  = 4500;
+#        EthernetInterface = "eth0";
+#    };
+#};
+#       vi /magichome/xruser/config/XRImDet.dyn
+#Update CP_WIFI_1, like below:
+#[CP_WIFI_1]
+
+#DetcommMedium.Type = String
+#DetcommMedium.Val = eth0
+#PowerSupplyInfo.Type = String
+#PowerSupplyInfo.Val = DCRegulated
+#DetectorConnectionStrategy.Type = Number
+#DetectorConnectionStrategy.Val = 0                     #change from 1 to 0
+#DetectorConnectionType.Type = String
+#DetectorConnectionType.Val = CONNECT_WIFI
+#SupportedDetectorTypes.Type = StringList
+#SupportedDetectorTypes.Val = { iDR }
+#       vi /magichome/xruser/bin/start.magic
+#Add these to the end:
+
+#to start detsim
+#sudo /magichome/xruser/detsim/bin/tablesim_iDR.sh
+
+#       vi /magichome/xruser/bin/kilall
+#Add these to the end:
+
+#to kill detsim
+#sudo /magichome/xruser/detsim/bin/kildetsim.sh
+#       vi /magichome/xruser/install/detector_fw/iDR/VersionInfo.txt
+
+#Change the content to 256f0612
+#       Restart App
+#Kilall;start.magic
+#       when you see detector boots up successfully, execute this command:
+#/magichome/xruser/bin/copyiDRCalFiles.sh
--- start-progress-if-not-exist/start_progress_if_not_exist.sh
+++ start-progress-if-not-exist/start_progress_if_not_exist.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-# usage:start_progress_if_not_exist.sh key start_command
+# Start the progress if the progress not exist. If already existed,do nothing.
+# usage : start_progress_if_not_exist.sh key start_command
 
 count=`ps -ef | grep $1 | grep -v "grep" | grep -v $0 | wc -l`
 #ps -ef | grep "$1" | grep -v "grep"
